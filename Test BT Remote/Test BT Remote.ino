671bc7c78a314dae926cb435049c27a4bfe19eab
CSAapps <43952184+CSAapps@users.noreply.github.com>
2021-10-17 18:09:52 +0530

    update code

--- Test BT Remote/Test BT Remote.ino
+++ Test BT Remote/Test BT Remote.ino
@@ -10,19 +10,9 @@ void setup(){
 void loop(){
  if (bt.available()) 
    Serial.write(bt.read());
- if (Serial.available()) 
-   bt.write(Serial.read());
 }
 
 /*
-This will send your PC serial monitor Input to app and app input to PC
-The app will recognize the end of the message from '\r' character.
-So select "Carriage return" or "Both  NL & CR" in the PC serial monitor.
-And also u should use println() instead of print() when make your own code with some commands. */
-//Ex:- 
-//bt.print("Hello");
-/*This doesn't show the msg in the app because doesn't contains '\r' int the end of message.
-so u have to code like below */
-//Ex:-
-//bt.println("Hello"); or bt.print("Hello\r");
-//Both show the output in the app because both containing '\r'
+This will print comands from the Blutooth remote as it is.
+Open the serial monitor in your PC to check.
+*/
