ff6da8417241f4409ef5251607e69b1c2b310721
CSAapps <43952184+CSAapps@users.noreply.github.com>
2022-01-13 14:13:12 +0530

    modified comment

--- Simple Switch/Simple Switch.ino
+++ Simple Switch/Simple Switch.ino
@@ -1,3 +1,5 @@
+//example code for the 'Simple Switch' remote in the app
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial bt(2, 3); // RX | TX
@@ -16,16 +18,3 @@ void loop() {
       digitalWrite(LED_BUILTIN, LOW);
   }
 }
-
-/*
-  This will send your PC serial monitor Input to app and app input to PC
-  The app will recognize the end of the message from '\r' character.
-  So select "Carriage return" or "Both  NL & CR" in the PC serial monitor.
-  And also u should use println() instead of print() when make your own code with some commands. */
-//Ex:-
-//bt.print("Hello");
-/*This doesn't show the msg in the app because doesn't contains '\r' int the end of message.
-  so u have to code like below */
-//Ex:-
-//bt.println("Hello"); or bt.print("Hello\r");
-//Both show the output in the app because both containing '\r'
